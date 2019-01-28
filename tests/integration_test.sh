00c0f58a506930f8e152efd829db9646a7a554f8
Will Htun <willhtun@cs130.org>
2019-01-28 15:36:32 -0800

    Please work
    Change-Id: Ibad9afeac53dfbd138d96afadba41b45af225297

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../build 
+cd /usr/src/projects/git-r-done/build 
 # Build the web server binary
 echo 'BUILDING'
 make clean
@@ -21,11 +21,11 @@ echo "http {
 #echo $PROCESS_ID
 #send server a request
 echo "SENDING SERVER REQUEST"
-curl -i http://localhost:8080 > /usr/src/projects/git-r-done/tests/integration_test_response
+curl -i http://localhost:8080 > ../tests/integration_test_response
 #curl -i -s GET / HTTP/1.1> /usr/src/projects/git-r-done/tests/integration_test_response
 echo "CHECKING REQUEST FOR CORRECTNESS"
 #check response of correctness
-DIFF=$(diff -w -B /usr/src/projects/git-r-done/tests/integration_expected_response /usr/src/projects/git-r-done/tests/integration_test_response)
+DIFF=$(diff -w -B ../tests/integration_expected_response ../tests/integration_test_response)
 EXIT_STATUS=$? 
 
 if [ "$EXIT_STATUS" -eq 0 ]
@@ -38,7 +38,7 @@ fi
 
 echo "SHUTTING DOWN"
 # Shutdown the webserver 
-pkill ./bin/server_main
+pkill server_main
 
 rm -f ../tests/integration_test_response
 
