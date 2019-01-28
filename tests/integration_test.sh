321ad6303cceb634e974d0147ade29a2545d38f7
Victor Fu <victorfu@cs130.org>
2019-01-28 22:06:47 +0000

    Debugged integration test script to work properly. Changed the CMake to include more test files.
    Change-Id: I52e32e06d2f7246368eaffc47dd33450f6405a83

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -3,6 +3,7 @@
 cd /usr/src/projects/git-r-done/build 
 # Build the web server binary
 echo 'BUILDING'
+make clean
 make
 echo "BUILD COMPLETE"
 
@@ -19,25 +20,26 @@ bin/server_main /usr/src/projects/git-r-done/tests/integration_test_config &
 
 #send server a request
 echo "SENDING SERVER REQUEST"
-curl -s http://localhost:8080 > /usr/src/projects/git-r-done/tests/integration_test_response
+curl -i http://localhost:8080 > /usr/src/projects/git-r-done/tests/integration_test_response
 #curl -i -s GET / HTTP/1.1> /usr/src/projects/git-r-done/tests/integration_test_response
 echo "CHECKING REQUEST FOR CORRECTNESS"
 #check response of correctness
-DIFF=$(diff /usr/src/projects/git-r-done/tests/integration_expected_response /usr/src/projects/git-r-done/tests/integration_test_response)
-EXIT_STATUS=$? #flag
+DIFF=$(diff -w -B /usr/src/projects/git-r-done/tests/integration_expected_response /usr/src/projects/git-r-done/tests/integration_test_response)
+EXIT_STATUS=$? 
 
 if [ "$EXIT_STATUS" -eq 0 ]
 then
     echo "SUCCESS: Integration test passed"
 else
     echo "FAILED: Integration test failed"
+    echo $DIFF
 fi 
 
 echo "SHUTTING DOWN"
 # Shutdown the webserver 
 pkill server_main
-make clean
-#rm -f /usr/src/projects/git-r-done/tests/integration_test_response
+
+rm -f /usr/src/projects/git-r-done/tests/integration_test_response
 
 # success (0) or failure (1)
 exit "$EXIT_STATUS"
