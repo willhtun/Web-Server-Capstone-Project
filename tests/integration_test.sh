1cbe8adf42b415abac8b280c21f5887130b3cbe1
jkwittmuss <jkwittmuss@cs130.org>
2019-01-28 16:03:57 -0800

    changed path in bash file
    Change-Id: I5343145a91b182752618b16263adbd1b59cd57d3

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /usr/src/project/build 
+cd /usr/src/project 
 # Build the web server binary
 echo 'BUILDING'
 make clean
@@ -13,19 +13,19 @@ echo "http {
     listen 8080;
     server_name 127.0.0.1;
   }
-}" > ../tests/integration_test_config
+}" > ./tests/integration_test_config
 
 #build the server with the config file
-./bin/server_main ../tests/integration_test_config & 
+./build/bin/server_main ./tests/integration_test_config & 
 #PROCESS_ID = $!
 #echo $PROCESS_ID
 #send server a request
 echo "SENDING SERVER REQUEST"
-curl -i http://localhost:8080 > ../tests/integration_test_response
+curl -i http://localhost:8080 > ./tests/integration_test_response
 #curl -i -s GET / HTTP/1.1> /usr/src/projects/git-r-done/tests/integration_test_response
 echo "CHECKING REQUEST FOR CORRECTNESS"
 #check response of correctness
-DIFF=$(diff -w -B ../tests/integration_expected_response ../tests/integration_test_response)
+DIFF=$(diff -w -B ./tests/integration_expected_response ./tests/integration_test_response)
 EXIT_STATUS=$? 
 
 if [ "$EXIT_STATUS" -eq 0 ]
@@ -40,7 +40,7 @@ echo "SHUTTING DOWN"
 # Shutdown the webserver 
 pkill server_main
 
-rm -f ../tests/integration_test_response
+rm -f ./tests/integration_test_response
 
 # success (0) or failure (1)
 exit "$EXIT_STATUS"
