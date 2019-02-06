67a3457e842f27b5f4b015fa8fad5cd8f60b41ef
Victor Fu <victorfu@cs130.org>
2019-02-06 02:33:01 +0000

    Add support for many mime types, 404 and 415 errors
    Change-Id: Ida19a015565bb49f232fe756ef7faa26f8718125

--- src/static_handler.cc
+++ src/static_handler.cc
@@ -20,35 +20,72 @@ RequestHandler::statuscode StaticHandler::HandleRequest(Request request, Respons
     if (dot_index != std::string::npos)
         fileextension = filename.substr(dot_index + 1, filename.length() - dot_index - 1);
 
+    //read in file
     std::string image;
     std::ifstream ifs(".." + ServerObject::staticfile_dir + "/" + filename, std::ios::in | std::ios::binary);
    
+   //if fail, give 404 error
+   if (!ifs)
+  {
+    response.SetStatus(Response::NOT_FOUND);
+    response.SetHeader("Content-Type", "text/plain");
+    response.SetBody("404: File not found");
+    return RequestHandler::NOT_FOUND;
+  }
     char buf[512];
     while (ifs.read(buf, sizeof(buf)).gcount() > 0) {
         image.append(buf, ifs.gcount());
     }
-
     ifs.close();
-
-    response.SetStatus(Response::OK);
-
+    
+    //text file MIMES
     if (fileextension == "html")
+        contenttype = "text/html";
+    else if (fileextension == "htm")
         contenttype = "text/html";
     else if (fileextension == "css")
         contenttype = "text/css";
+    else if (fileextension == "csv")
+        contenttype = "text/csv";
+    else if (fileextension == "txt")
+        contenttype = "text/plain";
+    else if (fileextension == "js")
+        contenttype = "text/javascript";
+    else if (fileextension == "xml")
+        contenttype = "text/xml";
+
+    //image file MIMES
     else if (fileextension == "jpg" || fileextension == "jpeg")
         contenttype = "image/jpeg";
     else if (fileextension == "png")
         contenttype = "image/png";
     else if (fileextension == "gif")
         contenttype = "image/gif";
+    else if (fileextension == "tif" || fileextension == "tiff")
+        contenttype = "image/tiff";
+
+    //application file MIMES
     else if (fileextension == "pdf")
         contenttype = "application/pdf";
     else if (fileextension == "zip")
         contenttype = "application/zip";
+    else if (fileextension == "rtf")
+        contenttype = "application/rtf";
+    else if (fileextension == "json")
+        contenttype = "application/json";
+    else if (fileextension == "doc")
+        contenttype = "application/msword";
+
     else
-        contenttype = "text/plain";
-    
+    {
+        response.SetStatus(Response::UNSUPPORTED_MEDIA_TYPE);
+        response.SetHeader("Content-Type", "text/plain");
+        response.SetBody("415: UNSUPPORTED_MEDIA_TYPE");
+        return RequestHandler::UNSUPPORTED_MEDIA_TYPE;
+    }
+
+    //send a correct response 
+    response.SetStatus(Response::OK);
     response.SetHeader("Content-Type", contenttype);
     response.SetHeader("Content-Length", std::to_string(image.length()));
     response.SetBody(image);
