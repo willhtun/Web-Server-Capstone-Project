056064582fda53e1d583529e329771a141a9b476
Victor Fu <victorfu@cs130.org>
2019-02-06 07:30:43 +0000

    help
    Change-Id: If4256293a6f443c807d10690f66fcf5f07fe9095

--- src/static_handler.cc
+++ src/static_handler.cc
@@ -22,7 +22,7 @@ RequestHandler::statuscode StaticHandler::HandleRequest(Request request, Respons
 
     //read in file
     std::string image;
-    std::ifstream ifs("usr/src/project" + ServerObject::staticfile_dir + "/" + filename, std::ios::in | std::ios::binary);
+    std::ifstream ifs("usr/source/projects" + ServerObject::staticfile_dir + "/" + filename, std::ios::in | std::ios::binary);
    
    //if fail, give 404 error
    if (!ifs.is_open())
@@ -30,7 +30,7 @@ RequestHandler::statuscode StaticHandler::HandleRequest(Request request, Respons
     response.SetStatus(Response::NOT_FOUND);
     response.SetHeader("Content-Type", "text/plain");
     response.SetHeader("Content-Length", std::to_string(19));
-    response.SetBody("404: File not found on path: usr/src/project" + ServerObject::staticfile_dir + "/" + filename);
+    response.SetBody("404: File not found");
     return RequestHandler::NOT_FOUND;
   }
     char buf[512];
