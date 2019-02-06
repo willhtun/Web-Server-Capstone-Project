e65e67a34c868e35cbf8c497ee025f10e5edba16
Will Htun <willhtun@cs130.org>
2019-02-05 23:56:53 -0800

    Fixed a test
    Change-Id: I2d891c3f93e70273362d173c3ee76317c2c5bb3a

--- tests/config_parser_test.cc
+++ tests/config_parser_test.cc
@@ -155,13 +155,6 @@ TEST_F(NginxConfigTest, ObjectConstruction) {
   EXPECT_EQ(ServerObject::port, 80);
 }
 
-// single block server test
-TEST_F(NginxConfigTest, SingleServerBlockTest) {
-  EXPECT_TRUE(parseFile("../tests/single_server_config"));
-  config_.GetServerObject();
-  EXPECT_EQ(ServerObject::port, 80);
-}
-
 // check valid port number
 TEST_F(NginxConfigTest, PortTest1) {
   EXPECT_TRUE(parseFile("../tests/echo_server_config"));
