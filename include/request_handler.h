c1e6e88ef37dd4915c4435b9dc8f5988ed9c755c
Konner <konnermacias@cs130.org>
2019-02-18 06:04:02 +0000

    make dispatch pretty
    Change-Id: I1873d9dda46834a7231bcd76d58c30eafd85a4f7

--- include/request_handler.h
+++ include/request_handler.h
@@ -11,54 +11,11 @@ class Response;
 
 class RequestHandler
 {
-public: 
-    /* Commented out as we refactor. Delete as necessary
-    enum statuscode
-    {
-        OK = 200,
-        CREATED = 201,
-        ACCEPTED = 202,
-        NO_CONTENT = 204,
-        MULTIPLE_CHOICES = 300,
-        MOVED_PERMANENTLY = 301,
-        MOVED_TEMPORARILY = 302,
-        NOT_MODIFIED = 304,
-        BAD_REQUEST = 400,
-        UNAUTHORIZED = 401,
-        FORBIDDEN = 403,
-        NOT_FOUND = 404,
-        UNSUPPORTED_MEDIA_TYPE = 415,
-        INTERNAL_SERVER_ERROR = 500,
-        NOT_IMPLEMENTED = 501,
-        BAD_GATEWAY = 502,
-        SERVICE_UNAVAILABLE = 503
-    };
-    */
-
+  public: 
     static RequestHandler* create(const NginxConfig& config, const std::string& path);
-
     virtual  std::unique_ptr<Response> HandleRequest(const Request& request);
 
-    protected: 
-
-    //what do we actually need here?
+  protected: 
     std::string path_;
     NginxConfig config_;
-
-
-    Request req;
-    Response response;
-
-
-
-
-
-
-    //virtual statuscode HandleRequest(Request request, Response& response) = 0;
-    // Response gather_response(Request request);
-/*
-private:
-    Response dispatch(std::string uri_type); // this function will allow us to create specialized request handling objects
-    std::string parse_uri(std::string uri_path);
-*/
 };
--- src/dispatcher.cc
+++ src/dispatcher.cc
@@ -2,33 +2,50 @@
 #include "handle_manager.h"
 #include "request_handler.h"
 
-Dispatcher::Dispatcher (NginxConfig* config) {
-  root_ = config->GetRoot();
-  config->ParseString(0, configTable_, handlerTable_);
+Dispatcher::Dispatcher (NginxConfig* config)
+{
+    root_ = config->GetRoot();
+    config->ParseString(0, configTable_, handlerTable_);
 }
 
-void Dispatcher::dispatch(Request* req) {
-  HandlerManager handlermanager_;
+void Dispatcher::dispatch(Request *req)
+{
+    /*
+        Determines name, and fires off HandlerManager to get a RequestHandler object.
+        This object will handle the request.
+    */
+    HandlerManager handlermanager_;
+    std::string name;
+    NginxConfig* nginxconfig;
+    std::string path = root_;
 
-  std::string full_url = req->uri_path();
-  int i = 1;
-  for (i; i < full_url.length(); i++) {
-    if (full_url[i] == '/')
-      break;
-  }
-  std::string url = full_url.substr(1,i-1); // extract uri 
+    // read full url
+    std::string full_url = req->uri_path();
+    int i = 1;
+    for (i; i < full_url.length(); i++)
+    {
+        if (full_url[i] == '/')
+        break;
+    }
 
-  std::map<std::string,std::string>::iterator it = handlerTable_.find(url); // find if handler type exists
-  if (it != handlerTable_.end()) {
-    std::string name = handlerTable_.find(url)->second;
-    NginxConfig* nginxconfig = configTable_.find(url)->second;
-  }
-  else {
-    std::string name = "404"; // placeholder
-    NginxConfig* nginxconfig = nullptr;
-  }
-  std::string path = root_;
+    // extract uri 
+    std::string uri = full_url.substr(1,i-1);
 
-  std::unique_ptr<RequestHandler> handler = handlermanager_.createByName(name, nginxconfig, path);
-  handler.handleRequest(req);
+    // check if handler type exists for this url
+    std::map<std::string,std::string>::iterator it = handlerTable_.find(uri);
+    if (it != handlerTable_.end())
+    {
+        // assign name
+        name = handlerTable_.find(uri)->second;
+        nginxconfig = configTable_.find(uri)->second;
+    }
+    else
+    {
+         // placeholder
+        name = "404";
+        nginxconfig = nullptr;
+    }
+
+    std::unique_ptr<RequestHandler> handler = handlermanager_.createByName(name, *nginxconfig, path);
+    handler->HandleRequest(*req);
 }
