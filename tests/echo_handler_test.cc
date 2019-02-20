34af42d4a8265c009664118f702442fcd395ac58
Will Htun <willhtun@cs130.org>
2019-02-19 23:12:44 -0800

    Added unit tests for the new classes
    Change-Id: I03102f0002b92e3c970416109474d498c1beadaa

--- tests/echo_handler_test.cc
+++ tests/echo_handler_test.cc
@@ -1,2 +1,38 @@
-#include "echo_handler.h"
 #include "gtest/gtest.h"
+#include "config_parser.h"
+#include "server.h"
+
+// define fixture
+class EchoConfigParserTest : public ::testing::Test {
+  protected:
+    NginxConfigParser parser_;
+    NginxConfig out_config_;
+
+    bool parseFile(std::string config_file) {
+      return parser_.Parse(config_file.c_str(), &out_config_);
+    }
+
+};
+
+// test given example
+TEST_F(EchoConfigParserTest, ExampleConfig) {
+  EXPECT_TRUE(parseFile("../tests/configs/example_config"));
+  // out_config should have indicated port
+  EXPECT_EQ(out_config_.GetPort(), 80);
+}
+
+// check big example
+TEST_F(EchoConfigParserTest, FullConfig) {
+  EXPECT_TRUE(parseFile("../tests/configs/full_config"));
+  std::cout << "PORTPORTPORT: " << std::to_string(out_config_.GetPort()) << std::endl;
+  EXPECT_EQ(out_config_.GetPort(), 80);
+}
+
+// check on separate large example for depth of validity
+TEST_F(EchoConfigParserTest, FullConfig2) {
+  EXPECT_TRUE(parseFile("../tests/configs/full_config2"));
+  EXPECT_EQ(out_config_.GetPort(), 80);
+}
+
+
+
