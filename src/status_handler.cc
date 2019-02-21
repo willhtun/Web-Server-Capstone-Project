289c02fdf6ca90f6a53c465d175d11895532380f
Victor Fu <victorfu@cs130.org>
2019-02-21 05:46:04 +0000

    Small visual change to the status webpage
    Change-Id: Idc2817fe2becf3c829ad20ad59215913c066dc68

--- src/status_handler.cc
+++ src/status_handler.cc
@@ -28,6 +28,15 @@ std::unique_ptr<Response> StatusHandler::HandleRequest(const Request& request)
     std::string body = "";
     body += "Status Page: git-r-done Server \n";
 
+    // bring in handler details
+    body += "\nRequest Handlers That Exist and Their Corresponding URL Prefixes:\n";
+    std::vector<std::string> myReqHandlers = config_.getReqHandlers();
+    for (std::vector<std::string>::const_iterator it = myReqHandlers.cbegin(); it != myReqHandlers.cend(); it++)
+    {
+        // TODO: specify handler and corresponding prefix
+        body += *it + "\n";
+    }
+
     std::cout << "Size of status entries: " << status_entries.size() << std::endl;
     //Get the total number of requests made
     int totalcalls = StatusObject::getStatusCalls();
@@ -37,16 +46,7 @@ std::unique_ptr<Response> StatusHandler::HandleRequest(const Request& request)
     for (std::vector<std::tuple<int,std::string,std::string>>::const_iterator it = status_entries.begin();
          it != status_entries.end(); it++)
     {
-        body += "Request URL: " + std::get<1>(*it) + " | Response Code: " + std::get<1>(*it) + " | Request Count: " + std::to_string(std::get<0>(*it)) + "\n";
-    }
-
-    // bring in handler details
-    body += "\nRequest Handlers That Exist and Their Corresponding URL Prefixes:\n";
-    std::vector<std::string> myReqHandlers = config_.getReqHandlers();
-    for (std::vector<std::string>::const_iterator it = myReqHandlers.cbegin(); it != myReqHandlers.cend(); it++)
-    {
-        // TODO: specify handler and corresponding prefix
-        body += *it + "\n";
+        body += "Request URL: " + std::get<1>(*it) + " | Response Code: " + std::get<2>(*it) + " | Request Count: " + std::to_string(std::get<0>(*it)) + "\n";
     }
 
     // set response data
