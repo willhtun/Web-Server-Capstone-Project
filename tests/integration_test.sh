d47a94ed03ae4fe1a2f074d2af42bef6b0f7c92f
Ertheo Siswadi <esiswadi@cs130.org>
2019-02-26 18:58:18 +0000

    first part of the proxy handler test
    Change-Id: Icc1e87cdd95779ab3d5680690dd45bc22bb5ccd7

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -10,9 +10,15 @@ echo "BUILD COMPLETE"
 #config for the server. Outputs config to file
 echo "http {
   server {
-    listen 80;
+    listen 8080;
     handler echo {
       url /echo;
+    }
+    handler proxy {
+      url /proxy1;
+      host www3.coolcoders.cs130.org;
+      path /static_1/a.txt;
+      port 80;
     }
     server_name 127.0.0.1;
   }
@@ -23,6 +29,7 @@ echo "http {
 #PROCESS_ID = $!
 #echo $PROCESS_ID
 #send server a request
+echo "TESTING ECHO HANDLER"
 echo "SENDING SERVER REQUEST"
 curl -i http://35.233.235.101/echo > ./tests/integration/integration_test_response
 #curl -i -s GET / HTTP/1.1> /usr/src/projects/git-r-done/tests/integration_test_response
@@ -33,9 +40,24 @@ EXIT_STATUS=$?
 
 if [ "$EXIT_STATUS" -eq 0 ]
 then
-    echo "SUCCESS: Integration test passed"
+    echo "SUCCESS: Echo handler integration test passed"
 else
-    echo "FAILED: Integration test failed"
+    echo "FAILED: Echo handler integration test failed"
+    echo $DIFF
+fi 
+
+echo "TESTING PROXY HANDLER"
+echo "SENDING SERVER REQUEST"
+curl -L localhost:8080/proxy1 > ./tests/integration/integration_test_proxy_response
+#check response of correctness
+DIFF=$(diff -w -B ./tests/integration/integration_expected_proxy_response ./tests/integration/integration_test_proxy_response)
+EXIT_STATUS=$? 
+
+if [ "$EXIT_STATUS" -eq 0 ]
+then
+    echo "SUCCESS: Proxy handler integration test passed"
+else
+    echo "FAILED: Proxy handler integration test failed"
     echo $DIFF
 fi 
 
