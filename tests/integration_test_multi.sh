20e9901cf8df8ba87eb112bdb188447a87402527
Victor Fu <victorfu@cs130.org>
2019-02-28 06:28:40 +0000

    Integration test tuning for multithreading
    Change-Id: I88d069ca9f7bd9ef7d766c1207b208f7df8ebb8c

--- tests/integration_test_multi.sh
+++ tests/integration_test_multi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd /usr/src/projects/git-r-done/build
+#cd build
 # Build the web server binary
-echo 'BUILDING'
+#echo 'BUILDING'
 #make clean
-make
-echo "BUILD COMPLETE"
+#make
+#echo "BUILD COMPLETE"
 
 #config for the server. Outputs config to file
 echo "http {
@@ -20,11 +20,12 @@ echo "http {
 
 #build the server with the config file
 ../build/bin/server_main ../tests/integration/integration_test_config & 
-#PROCESS_ID = $!
-#echo $PROCESS_ID
+
+sleep 3
+
 #send server a request
 echo "SENDING SERVER REQUEST"
-curl -i http://127.0.0.1/echo > ../tests/integration/integration_test_response
+curl -i localhost:8070/echo > ../tests/integration/integration_test_response
 #curl -i -s GET / HTTP/1.1> /usr/src/projects/git-r-done/tests/integration_test_response
 echo "CHECKING REQUEST FOR CORRECTNESS"
 #check response of correctness
@@ -42,13 +43,13 @@ fi
 echo "TESTING MULTIPLE THREADS"
 
 exec 3<>/dev/tcp/localhost/8070
-echo -e "GET / HTTP/1.1\r\nhost: localhost\r\nConn" >&3
+echo -e "GET /echo HTTP/1" >&3
 
 #Testing 404
-curl -i http://35.233.235.101/missing > ./tests/integration/integration_test_response2
+curl -i localhost:8070/missing > ../tests/integration/integration_test_response2
 
 #check response of correctness
-DIFF=$(diff -w -B ./tests/integration/integration_expected_response2 ./tests/integration/integration_test_response2)
+DIFF=$(diff -w -B ../tests/integration/integration_expected_response2 ../tests/integration/integration_test_response2)
 EXIT_STATUS=$? 
 
 if [ "$EXIT_STATUS" -eq 0 ]
@@ -59,12 +60,14 @@ else
     echo $DIFF
 fi 
 
-#Second half of 
-echo -e "ection: close\r\n\r\n" >&3
-cat <&3 > ./tests/integration/integration_test_response3
+
+echo "CHECKING SECOND HALF OF ECHO"
+echo -e ".1\r\nhost: localhost\r\nConnection: close\r\n\r\n" >&3
+
+cat <&3 > ../tests/integration/integration_test_response3
 
 #check response of correctness
-DIFF=$(diff -w -B ./tests/integration/integration_expected_response3 ./tests/integration/integration_test_response3)
+DIFF=$(diff -w -B ../tests/integration/integration_expected_response3 ../tests/integration/integration_test_response3)
 EXIT_STATUS=$? 
 
 if [ "$EXIT_STATUS" -eq 0 ]
