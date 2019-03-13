728987a8ff568d7bcdfecc05b51d30b3f4428d1a
Konner <konnermacias@cs130.org>
2019-03-13 03:54:01 +0000

    fix master token
    Change-Id: I79c9abe60c8a9946bfbe05ac89fff309751f71a6

--- src/meme_handler.cc
+++ src/meme_handler.cc
@@ -127,7 +127,7 @@ bool MemeHandler::verifyTokenAuthentication(Request req)
     std::map<std::string, std::string> memeMap = parseRequestBody(req.body());
     
     // master token check
-    if (memeMap["access-token"].length() > 0 && memeMap["access-token"] == "partial=!complete") { return true; }
+    if (memeMap["access-token"].length() > 0 && memeMap["access-token"] == "partialnotcomplete") { return true; }
     // check that access token has length and our - in it
     bool isAuthorized = (memeMap["access-token"].length() > 0 && (memeMap["access-token"].find("-") != std::string::npos));
     if (isAuthorized)
